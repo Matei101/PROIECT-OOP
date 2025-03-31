5037e28d54c54d9810a9d1fed2eee05a50b446a2
Matei <petrucmatei@gmail.com>
2025-03-31 19:14:26 +0300

    update

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ private:
     std::vector<Player> players;
 
 public:
-    Team(const std::string& name) : name(name) {}
+    explicit Team(const std::string& name) : name(name) {}
 
     Team(const Team& other) : name(other.name), players(other.players) {}
 
