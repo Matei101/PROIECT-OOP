fbe1b48e8160ba2469842edbc871748ae6fada2b
Matei <petrucmatei@gmail.com>
2025-07-03 15:32:48 +0300

    update

--- Match.cpp
+++ Match.cpp
@@ -1,2 +1,20 @@
-
 #include "Match.h"
+
+Match::Match(const Team& t1, const Team& t2)
+    : team1(t1), team2(t2), score1(0), score2(0)
+{}
+
+void Match::simulate() {
+    double avg1 = team1.averageSkill();
+    double avg2 = team2.averageSkill();
+    score1 = static_cast<int>(avg1) % 5 + rand() % 3;
+    score2 = static_cast<int>(avg2) % 5 + rand() % 3;
+}
+
+void Match::showResult() const {
+    std::cout << "Rezultat meci: " << team1.getName() << " " << score1
+              << " - " << score2 << " " << team2.getName() << "\n";
+}
+
+int Match::getScore1() const { return score1; }
+int Match::getScore2() const { return score2; }
--- Match.h
+++ Match.h
@@ -1,13 +1,24 @@
-
 #ifndef MATCH_H
 #define MATCH_H
 
-
+#include <iostream>
+#include <cstdlib>
+#include "Team.h"
 
 class Match {
+public:
+    Match(const Team& t1, const Team& t2);
 
+    void simulate();
+    void showResult() const;
+    int getScore1() const;
+    int getScore2() const;
+
+private:
+    const Team& team1;
+    const Team& team2;
+    int score1;
+    int score2;
 };
 
-
-
-#endif //MATCH_H
+#endif // MATCH_H
